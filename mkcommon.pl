331e7364a92381ca8dab83d247ba67d1472b8b3a
Paul E. Black <paul.black@nist.gov>
2001-11-30 18:26:18 -0500

    Abstract out the functionality to add a term to the in-memory dictionary so we can create terms from file entries or from AKA names.  Make other changes needed to generate AKA entries.  Count the number of terms in the index separately from the number of terms files read.

--- mkcommon.pl
+++ mkcommon.pl
@@ -1,6 +1,6 @@
-# $Id: mkcommon.pl,v 1.4 2001/11/15 15:41:10 black Exp $
+# $Id: mkcommon.pl,v 1.5 2001/11/30 18:26:18 black Exp $
 # *created  "Tue Apr  3 15:51:02 2001" *by "Paul E. Black"
-# *modified "Thu Sep 13 09:43:46 2001" *by "Paul E. Black"
+# *modified "Fri Nov 30 13:15:50 2001" *by "Paul E. Black"
 #
 # Common definitions and routines for format and indexing terms.
 #
@@ -280,6 +280,130 @@ sub readConfigFiles {
 #
 #------------------------------------------------------------------------
 
+sub addToDictionary (\%) {
+    undef %entry; # make sure nothing is left over
+    my(%entry, $entryRef);
+    $entryRef = shift;
+    %entry = %$entryRef;
+
+    #for $fld (keys %entry) {
+    #	print "\$entry\{$fld} = $entry{$fld}\n";
+    #}
+
+    # audit the entry
+    if ($entry{NAME} =~ / $/) {
+	print "NAME <<$entry{NAME}>> has a trailing blank in $entryFile\n";
+    }
+    if (defined $entry{TYPE}) {
+	my($etype) = $entry{TYPE};
+	if (! defined $types{$etype}) {
+	    print "Unknown TYPE $etype in $entryFile\n";
+	}
+    }
+    if (defined $entry{AREA}) {
+	my($earea) = $entry{AREA};
+	if (! defined $areas{$earea}) {
+	    print "Unknown AREA $earea in $entryFile\n";
+	}
+    }
+    if (defined $entry{AUTHOR}) {
+	foreach $initials (split / *, */, $entry{AUTHOR}) {
+	    if (! defined $authors{$initials}) {
+		print "Unknown AUTHOR $initials in $entryFile\n";
+	    }
+	}
+    }
+
+    # NAME (ename in other places) is original name, e.g.,
+    #		BB$\alpha<sub>2</sub>$ tree
+    # XNAME is name without LaTeX markers to lookup cross references.
+    #		BB\alpha<sub>2</sub> tree
+    # DNAME is display name, e.g.,
+    #		BB<src img="alpha.gif"><sub>2</sub> tree
+    # TNAME is plain-text name, e.g., without HTML or LaTeX markers.
+    #		BBalpha2 tree
+    # alphname HERE is name used to alphabetize, e.g.,
+    #		BBALPHATWOTREE
+    #		bTHETA (where b is a blank)
+
+    # save a version of the entry name without LaTeX markers for lookup
+    ($entry{XNAME} = $entry{NAME}) =~ s/\$([^\$]*)\$/$1/go;
+
+    # save a rich display version of the entry name
+    rewriteLatex($entry{DNAME} = $entry{NAME});
+
+    # save a plain text version of the entry name, i.e., without HTML
+    my $tname;
+    ($tname = $entry{NAME}) =~ s/<[^>]*>//go;
+    $tname =~ s/\$([^\$]*)\$/$1/go;
+    $tname =~ s/\\//go;
+    ($entry{TNAME}) = $tname;
+
+    # figure out the name to alphabetize this with
+    my $alphaName = $entry{NAME};
+    print STDERR "." if ($verbose); # progress indicator
+    # index some numbers and symbols as words
+    $alphaName =~ s/\b0\b/zero/g;
+    $alphaName =~ s/\b1\b/one/g;
+    $alphaName =~ s/\b2\b/two/g;
+    $alphaName =~ s/\b3\b/three/g;
+    $alphaName =~ s/\b4\b/four/g;
+    $alphaName =~ s/[*]/star/go;
+    $alphaName =~ s/[+]/plus/go;
+    # remove any embedded html
+    $alphaName =~ s/<[^>]*>//go;
+    $startsWithSpecial = ($alphaName =~ /^\$\\/);
+    # make all letters upper case and remove any remaining non-letter
+    $alphaName =~ tr/A-Za-z\0-\377/A-ZA-Z/d;
+    if ($startsWithSpecial) {
+	# prepend a space so names with special characters come first
+	$alphaName = " $alphaName";
+    }
+    # make sure entry name is unique
+    while (defined $entries{$alphaName}) {
+	$alphaName .= "Z";
+    }
+
+    #print "NAME is $entry{NAME}\n"
+    #print "XNAME is $entry{XNAME}\n";
+    #print "DNAME is $entry{DNAME}\n";
+    #print "TNAME is $entry{TNAME}\n";
+    #print "alphaName is $alphaName\n";
+
+    # assign a file name if necessary (for AKA and WEB)
+    if (! defined $entry{FILENM}) {
+	$entry{FILENM} = $tname;
+
+	# change all-caps words to all-lower case
+	if ($entry{FILENM} =~ /^[A-Z]+$/) {
+	    $entry{FILENM} =~ tr/A-Z/a-z/;
+	}
+
+	# capitalize words following spaces (change "..se na.." to "..seNa..")
+	while ($entry{FILENM} =~ /( [a-z])/) {
+	    my $patrn = $1;
+	    (my $subst = $patrn) =~ tr/a-z/A-Z/;
+	    $subst =~ s/ //;
+	    #print "\n$entry{FILENM}\n";	# before
+	    $entry{FILENM} =~ s/$patrn/$subst/g;
+	    #print "$entry{FILENM}\n";		# after
+	}
+
+	# remove dashes and remaining spaces
+	$entry{FILENM} =~ s/[- ]//go;
+    }
+
+    # remember this entry to find cross references
+    $entriesForXref{$entry{XNAME}} = $alphaName;
+
+    # save as a hash-of-hashes
+    for $fld (keys %entry) {
+	$entries{$alphaName}{$fld} = $entry{$fld};
+    }
+
+    $numEntriesInIndex++;
+}
+
 sub readTermEntries {
     print STDERR "Reading terms" if ($verbose);
 
@@ -289,7 +413,9 @@ sub readTermEntries {
     #print @entries;
 
     # count the number of entries read
-    $numEntriesRead = 0;
+    my $numEntriesRead = 0;
+    # count the entries in the index.  This adds AKA's
+    $numEntriesInIndex = 0;
 
     # Step IIb: read each entry file and save the properties
     while ($entryFile = shift @entries) {
@@ -327,52 +453,56 @@ sub readTermEntries {
 
 	$numEntriesRead++;
 
-	# save the base of the file name (to create a parallel HTML page)
-	($thisEntry{BASEFILENM}) = ($entryFile =~ m|.*/([^.]*)|o);
+	# save the base of the file name (FILENM is used to create an HTML 
+	# page, and SRCFILE is used to check for modification)
+	($thisEntry{FILENM}) = ($entryFile =~ m|.*/([^.]*)|o);
+	$thisEntry{SRCFILE} = $thisEntry{FILENM};
 
 	# if no NAME given, make one from the file name
 	if (! defined $thisEntry{NAME}) {
 	    print "No entry NAME given in $entryFile\n";
 	    # assign a default
-	    ($thisEntry{NAME}) = ($thisEntry{BASEFILENM});
-	} else {
-	    if ($thisEntry{NAME} =~ / $/) {
-		print "NAME <<$thisEntry{NAME}>> has a trailing blank in $entryFile\n";
-	    }
+	    ($thisEntry{NAME}) = ($thisEntry{FILENM});
 	}
 
-	# audit the entry
-	if (defined $thisEntry{TYPE}) {
-	    my($etype) = $thisEntry{TYPE};
-	    if (! defined $types{$etype}) {
-		print "Unknown TYPE $etype in $entryFile\n";
-	    }
-	}
-	if (defined $thisEntry{AREA}) {
-	    my($earea) = $thisEntry{AREA};
-	    if (! defined $areas{$earea}) {
-		print "Unknown AREA $earea in $entryFile\n";
-	    }
-	}
-	if (defined $thisEntry{AUTHOR}) {
-	    foreach $initials (split / *, */, $thisEntry{AUTHOR}) {
-		if (! defined $authors{$initials}) {
-		    print "Unknown AUTHOR $initials in $entryFile\n";
-		}
-	    }
-	}
-
-	# add entries for AKA (Also Known As) names and WEB variants
+	# add entries for AKA (Also Known As) names
 	if (defined $thisEntry{AKA}) {
+
 	    my($ealiases) = $thisEntry{AKA};
 	    #print "$thisEntry{NAME} is also known as";
 	    foreach $aka (split /^ *{?|}?, *{?|}? *$/, $ealiases) {
 		next if ($aka eq ""); # above RE may start with a null split
-		#print " '$aka'"; # for debugging
-		# SKIMP create entry in database for this
+
+		#
+		# create entry in database for this
+		#
+
+		my $akaEntry = {};
+		$akaEntry{NAME} = $aka;
+		$akaEntry{DEFN} = "See \{$thisEntry{NAME}}.";
+		$akaEntry{SRCFILE} = $thisEntry{SRCFILE};
+		if (defined $thisEntry{TYPE}) {
+		    $akaEntry{TYPE} = $thisEntry{TYPE};
+		}
+		if (defined $thisEntry{AREA}) {
+		    $akaEntry{AREA} = $thisEntry{AREA};
+		}
+		if (defined $thisEntry{IMPL}) {
+		    $akaEntry{IMPL} = $thisEntry{IMPL};
+		}
+		if (defined $thisEntry{MODIFIED}) {
+		    $akaEntry{MODIFIED} = $thisEntry{MODIFIED};
+		}
+		if (defined $thisEntry{AUTHOR}) {
+		    $akaEntry{AUTHOR} = $thisEntry{AUTHOR};
+		}
+
+		addToDictionary %akaEntry;
 	    }
 	    #print "\n";
 	}
+
+	# add entries for WEB variants
 	if (defined $thisEntry{WEB}) {
 	    my($webaliases) = $thisEntry{WEB};
 	    #print "$thisEntry{NAME} has variant(s) for web of";
@@ -384,67 +514,11 @@ sub readTermEntries {
 	    #print "\n";
 	}
 
-	# NAME (ename in other places) is original name, e.g.,
-	#		BB$\alpha<sub>2</sub>$ tree
-	# XNAME is name without LaTeX markers to lookup cross references.
-	#		BB\alpha<sub>2</sub> tree
-	# DNAME is display name, e.g.,
-	#		BB<src img="alpha.gif"><sub>2</sub> tree
-	# TNAME is plain-text name, e.g., without HTML or LaTeX markers.
-	#		BBalpha2 tree
-	# alphname HERE is name used to alphabetize, e.g.,
-	#		BBALPHATWOTREE
-
-	# save a version of the entry name without LaTeX markers for lookup
-	($thisEntry{XNAME} = $thisEntry{NAME}) =~ s/\$([^\$]*)\$/$1/go;
-
-	# save a rich display version of the entry name
-	rewriteLatex($thisEntry{DNAME} = $thisEntry{NAME});
-
-	# save a plain text version of the entry name, i.e., without HTML
-	($tname = $thisEntry{NAME}) =~ s/<[^>]*>//go;
-	$tname =~ s/\$([^\$]*)\$/$1/go;
-	$tname =~ s/\\//go;
-	($thisEntry{TNAME}) = $tname;
-
-	# add this to an "index" for later sorting and printing
-	my $alphname = $thisEntry{NAME};
-	print STDERR "." if ($verbose); # progress indicator
-	# index some numbers and symbols as words
-	$alphname =~ s/\b0\b/zero/g;
-	$alphname =~ s/\b1\b/one/g;
-	$alphname =~ s/\b2\b/two/g;
-	$alphname =~ s/\b3\b/three/g;
-	$alphname =~ s/\b4\b/four/g;
-	$alphname =~ s/[*]/star/go;
-	$alphname =~ s/[+]/plus/go;
-	# remove any embedded html
-	$alphname =~ s/<[^>]*>//go;
-	$startsWithSpecial = ($alphname =~ /^\$\\/);
-	# make all letters upper case and remove any remaining non-letter
-	$alphname =~ tr/A-Za-z\0-\377/A-ZA-Z/d;
-	if ($startsWithSpecial) {
-	    # prepend a space so names with special characters come first
-	    $alphname = " $alphname";
-	}
-	# make sure entry name is unique
-	while (defined $entries{$alphname}) {
-	    $alphname .= "Z";
-	}
-	# save as a hash-of-hashes
-	for $fld (keys %thisEntry) {
-	    $entries{$alphname}{$fld} = $thisEntry{$fld};
-	}
-
-	#print "NAME is $thisEntry{NAME}\nTNAME is $thisEntry{TNAME}\n";
-	#print "DNAME is $thisEntry{DNAME}\nalphname is $alphname\n";
-	#print "XNAME is $thisEntry{XNAME}\n";
-	# remember this entry to check cross references
-	$entriesForXref{$thisEntry{XNAME}} = $alphname;
+	addToDictionary %thisEntry;
     }
     print STDERR "\n" if ($verbose);
 
-    print "Read $numEntriesRead terms\n";
+    print "Read $numEntriesRead terms.  $numEntriesInIndex terms in index.\n";
 }
 
 # end of $Source: /home/black/DADS/dads/RCS/mkcommon.pl,v $
